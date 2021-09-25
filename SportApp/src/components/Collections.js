f0f37e64566f7abd72af13df1f77235a55fc7dee
maedeh <maedehmohebbie@gmail.com>
2021-09-25 15:18:32 +0330

    c

--- SportApp/src/components/Collections.js
+++ SportApp/src/components/Collections.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import axios from "axios"
+import Post from './Post';
+import Styles from './Collections.module.css';
 
 class Collections extends Component {
-    constructor(){
-        super();
-        this.state={}
+    constructor(props){
+        super(props);
+        this.state={products:[]}
+    }
+    componentDidMount(){
+            axios.get(' https://wger.de/api/v2/exerciseimage')
+            .then(response=>this.setState({products:response.data.results}))
     }
     render() {
+        const {products}=this.state;
         return (
-            <div>
-               j 
+            <div className={Styles.container}>
+                {products.length?products.map(product=><Post key={product.id} image={product.image}  />):<h1>Loading...</h1>}
+                {}
             </div>
         );
     }
